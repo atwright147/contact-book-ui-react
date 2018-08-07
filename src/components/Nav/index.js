cafb748174982284d446c7c1afe7cdc565297839
Andy Wright <atwright147@gmail.com>
2018-08-07 21:46:23 +0100

    add font-awesome and icon nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router';
 
 const Nav = () => (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/contacts">Contacts</Link>
-        <Link to="/contact/create">Create a Contact</Link>
+        <Link to="/"><i className="fa fa-fw fa-4x fa-home"></i></Link>
+        <Link to="/contacts"><i className="fa fa-fw fa-4x fa-address-book"></i></Link>
+        <Link to="/contact/create"><i className="fa fa-fw fa-4x fa-user-plus"></i></Link>
     </nav>
 );
 
