105a59e27940df7a946e2064db26af53ec8cae29
Andy Wright <atwright147@gmail.com>
2018-08-08 22:49:20 +0100

    switch to arrow functions

--- src/actions/contactsActions.js
+++ src/actions/contactsActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getContactsAll = () => {
-    return function(dispatch) {
+export const getContactsAll = () =>
+    (dispatch) => 
         axios
             .get('/api/contacts')
             .then((res) => {
